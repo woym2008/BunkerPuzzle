1c39623afc1324a66e4707352caf70bf09df8c3b
woym2008 <woym-zf@hotmail.com>
2019-10-21 23:27:07 +0800

    create entitymanager

---
+++ Treasure/Assets/Code/Battlefield/Entity/EntityManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EntityManager
+{
+    private List<EntityBase> _entitys;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
