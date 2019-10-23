23ed53144b35b28878918084f2bee0c15d1a29b6
woym2008 <woym-zf@hotmail.com>
2019-10-24 00:06:20 +0800

    加了个脚本控制tile的精灵图创建出来后再受mask影响

---
+++ Treasure/Assets/Code/Battlefield/TileSpriteController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSpriteController : MonoBehaviour
+{
+    SpriteRenderer _renderer;
+    private void Awake()
+    {
+        _renderer = this.gameObject.GetComponent<SpriteRenderer>();
+
+        _renderer.maskInteraction = SpriteMaskInteraction.VisibleInsideMask;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
