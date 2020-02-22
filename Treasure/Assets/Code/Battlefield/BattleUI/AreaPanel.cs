8ab98495ac95ad6676f15850e6909e8a1ade8b94
woym2008 <woym-zf@hotmail.com>
2020-02-22 23:56:12 +0800

    关卡选择ui，可以变换区域了

--- Treasure/Assets/Code/Battlefield/BattleUI/AreaPanel.cs
+++ Treasure/Assets/Code/Battlefield/BattleUI/AreaPanel.cs
@@ -12,6 +12,12 @@ namespace Bunker.Game
         ScrollRect _levelList;
         Transform _buttonsParent;
         GameObject _btnPrefab;
+        GameObject _areaFrame;
+
+        GameObject _currentArea;
+
+        Button _nextBtn;
+        Button _preBtn;
 
         const string buttonPathBase = "UI/Icon";
         public override void OnBegin()
@@ -22,11 +28,20 @@ namespace Bunker.Game
 
             _buttonsParent = _levelList.content;
 
-            _btnPrefab = _buttonsParent.GetChild(0).gameObject;
+            _btnPrefab = _buttonsParent.Find("LevelBtn").gameObject;
+            _areaFrame = _buttonsParent.Find("AreaFrame").gameObject;
             _btnPrefab.transform.parent = _buttonsParent.parent;
+            _areaFrame.transform.parent = _buttonsParent.parent;
             _btnPrefab.SetActive(false);
+            _areaFrame.SetActive(false);
 
             _buttonsParent.DetachChildren();
+
+            _nextBtn = _transform.Find("NextButton").GetComponent<Button>();
+            _preBtn = _transform.Find("PreButton").GetComponent<Button>();
+
+            _nextBtn.onClick.AddListener(NextArea);
+            _preBtn.onClick.AddListener(PreArea);
         }
 
         public override void OnOpen(params object[] datas)
@@ -34,6 +49,21 @@ namespace Bunker.Game
             base.OnOpen(datas);
             string[] levels = (string[])datas[0];
             int areaID = (int)datas[1];
+            OpenArea(areaID, levels);
+        }
+
+        public override void OnClose()
+        {
+            CloseArea();
+            base.OnClose();
+        }
+
+        void OpenArea(int areaID, string[] levels)
+        {
+            var areaframe = GameObject.Instantiate(_areaFrame);
+            _currentArea = areaframe;
+            _currentArea.SetActive(true);
+            areaframe.transform.parent = _buttonsParent;
             foreach (var level in levels)
             {
                 var btn = GameObject.Instantiate(_btnPrefab).GetComponent<Button>();
@@ -41,13 +71,13 @@ namespace Bunker.Game
                 btnctrl.SetName(level);
                 string name = string.Format("{0}/{1}", buttonPathBase, level);
                 var sp = Resources.Load<Sprite>(name);
-                if(sp != null)
+                if (sp != null)
                 {
                     btn.image.sprite = sp;
                 }
 
                 _levelButtons.Add(btn);
-                btn.transform.parent = _buttonsParent;
+                btn.transform.parent = areaframe.transform;
 
                 btn.onClick.AddListener(delegate { OnClickBtn(int.Parse(level), areaID); });
 
@@ -55,15 +85,38 @@ namespace Bunker.Game
             }
         }
 
-        public override void OnClose()
+        void CloseArea()
         {
             _buttonsParent.DetachChildren();
-            foreach(var btn in _levelButtons)
+            foreach (var btn in _levelButtons)
             {
                 GameObject.Destroy(btn.gameObject);
             }
             _levelButtons.Clear();
-            base.OnClose();
+
+            GameObject.Destroy(_currentArea);
+        }
+
+        void NextArea()
+        {
+            var levels = LevelManager.getInstance.GetAreaLevels(LevelManager.getInstance.CurArea + 1);
+            if(levels != null)
+            {
+                CloseArea();
+                OpenArea(LevelManager.getInstance.CurArea + 1, levels);
+                LevelManager.getInstance.CurArea++;
+            }
+        }
+
+        void PreArea()
+        {
+            var levels = LevelManager.getInstance.GetAreaLevels(LevelManager.getInstance.CurArea - 1);
+            if (levels != null)
+            {
+                CloseArea();
+                OpenArea(LevelManager.getInstance.CurArea - 1, levels);
+                LevelManager.getInstance.CurArea--;
+            }
         }
 
         public void OnClickBtn(int level, int area)
