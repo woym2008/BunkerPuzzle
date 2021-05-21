e43bcdc0f42261eed86536fc10aacbdb78ea092c
woym2008 <woym-zf@hotmail.com>
2021-05-21 18:32:48 +0800

    炸弹和锁和石头的资源替换

--- Treasure/Assets/Code/Battlefield/Tile/BombTile.cs
+++ Treasure/Assets/Code/Battlefield/Tile/BombTile.cs
@@ -25,7 +25,10 @@ namespace Bunker.Game
             base.Init(additionalData);
             isBroken = false;
 
-            if(additionalData != null && additionalData != "")
+            isTimeBomb = false;
+            _timerValue = 3;
+
+            if (additionalData != null && additionalData != "")
             {
                 Debug.LogError(additionalData);
                 var datas = additionalData.Split(';');
@@ -39,22 +42,30 @@ namespace Bunker.Game
                 var turnmodule = ModuleManager.getInstance.GetModule<BattleTurnsModule>();
                 turnmodule.Notifications += OnNextTurn;
 
-
-                if (isTimeBomb)
-                {
-                    var timeBoardobj = _object.transform.Find("TimeBoard").gameObject;
-
-                    _timeBoard = timeBoardobj.GetComponent<SpriteRenderer>();
-
-                    ShowTimeValue(_timerValue);
-                }
             }
-            
+
+
+            if (isTimeBomb)
+            {
+                var timeBoardobj = _object.transform.Find("TimeBoard").gameObject;
+
+                _timeBoard = timeBoardobj.GetComponent<SpriteRenderer>();
+
+                ShowTimeValue(_timerValue);
+
+                var btnobj = _object.transform.Find("btn").gameObject;
+                btnobj.SetActive(false);
+            }
+            else
+            {
+                var timeBoardobj = _object.transform.Find("TimeBoard").gameObject;
+                timeBoardobj.SetActive(false);
+            }
         }
 
         void OnNextTurn(CTurn turn)
         {
-            if(turn.GetType() == typeof(PlayerTurn))
+            if (turn is PlayerTurn)
             {
                 PassTurn();
             }
@@ -78,14 +89,14 @@ namespace Bunker.Game
 
         void ShowTimeValue(int value)
         {
-            Debug.Log("Texture/Tile/BombTile/bombtime_" + _timerValue);
+            Debug.Log("Texture/Tile/BombTile/tnt_counter_" + _timerValue);
             //var tex = Resources.Load<Texture2D>("Texture/Tile/BombTile/bombtime_" + _timerValue);
             //Sprite sp = Sprite.Create(tex, _timeBoard.sprite.textureRect, new Vector2(0.5f, 0.5f));
             //var obj = Resources.LoadAll("Texture/Tile/BombTile/bombtime_" + _timerValue);
-            var sps = Resources.LoadAll<Sprite>("Texture/Tile/BombTile/bombtime");
+            var sps = Resources.LoadAll<Sprite>("Texture/Tile/BombTile/tnt_counter");
             try
             {
-                var name = "bombtime_" + _timerValue;
+                var name = "tnt_counter_" + _timerValue;
                 foreach (var sp in sps)
                 {
                     Debug.Log(sp.name);
@@ -107,18 +118,28 @@ namespace Bunker.Game
 
         public override void UpdateSortingOrder()
         {
-            //add by wwh
-            if (_timeBoard != null)
+            base.UpdateSortingOrder();
+
+            //if (_timeBoard != null)
+            //{
+            //    var kid = _object.transform.GetChild(0);
+            //    var sr = kid.GetComponent<SpriteRenderer>();
+            //    _timeBoard.sortingOrder = sr.sortingOrder + 1;
+            //}
+
+            if (_object != null && _object.transform.childCount > 0)
             {
-                var kid = _object.transform.GetChild(0);
-                var sr = kid.GetComponent<SpriteRenderer>();
-                _timeBoard.sortingOrder = sr.sortingOrder + 1;
+                foreach(Transform kid in _object.transform)
+                {
+                    var sr = kid.GetComponent<SpriteRenderer>();
+                    sr.sortingOrder = ParentGrid.RowID * 2;
+                }
             }
         }
 
         protected override int TileSize
         {
-            get { return 1; }
+            get { return 2; }
         }
 
         public override bool CanWalk()
